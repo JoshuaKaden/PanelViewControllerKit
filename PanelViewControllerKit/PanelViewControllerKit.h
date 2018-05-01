15e7e64456ebc12cd085d70b3920e846aa27c076
Joshua Kaden <jkaden@doitt.nyc.gov>
2018-05-01 14:29:48 -0400

    Initial Commit

---
+++ PanelViewControllerKit/PanelViewControllerKit.h
@@ -0,0 +1,19 @@
+//
+//  PanelViewControllerKit.h
+//  PanelViewControllerKit
+//
+//  Created by Kaden, Joshua on 5/1/18.
+//  Copyright © 2018 NYC DoITT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PanelViewControllerKit.
+FOUNDATION_EXPORT double PanelViewControllerKitVersionNumber;
+
+//! Project version string for PanelViewControllerKit.
+FOUNDATION_EXPORT const unsigned char PanelViewControllerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PanelViewControllerKit/PublicHeader.h>
+
+
